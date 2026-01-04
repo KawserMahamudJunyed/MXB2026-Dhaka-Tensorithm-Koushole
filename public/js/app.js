ba7967026f1671edfa963ce001b870b9828e12c2
KawserMahamudJunyed <kawsermahamudjunyed@gmail.com>
2026-01-04 13:23:41 +0600

    Update

--- public/js/app.js
+++ public/js/app.js
@@ -275,10 +275,10 @@ function toggleAuthPassword() {
     const icon = document.getElementById('auth-eye');
     if (input.type === 'password') {
         input.type = 'text';
-        icon.className = 'fa-regular fa-eye-slash text-lg';
+        icon.className = 'fa-regular fa-eye-slash text-sm';
     } else {
         input.type = 'password';
-        icon.className = 'fa-regular fa-eye text-lg';
+        icon.className = 'fa-regular fa-eye text-sm';
     }
 }
 
